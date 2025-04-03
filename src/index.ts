12486c3a4cc1d0720118cbf5343b53090398b85f
Matt Redmond <matt.redmond@katalyst.com.au>
2025-04-03 15:12:21 +1030

    Appify it

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static('.'));
+app.use("/app", express.static('./src/app'));
+
+app.get("/healthz", handlerReadiness)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function handlerReadiness(req: Request, res: Response) {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK")
+}
