1097abe651fe02a705278f95cb278822a4f4fc12
Matt Redmond <matt.redmond@katalyst.com.au>
2025-04-07 22:08:24 +0930

    Add handler to validate chirp length

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ import middlewareMetricsInc from "./middleware/middleware-metrics-inc.js";
 const app = express();
 const PORT = 8080;
 
-
 app.use("/app", middlewareMetricsInc, express.static('./src/app'));
 app.use(middlewareLogResponses);
 
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerValidateChirp);
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerResetMetrics);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
@@ -39,4 +40,29 @@ function handlerResetMetrics(req: Request, res: Response) {
   res.set("Content-Type", "text/plain; charset=utf-8");
   config.fileserverHits = 0;
   res.send("OK")
+}
+
+function handlerValidateChirp(req: Request, res: Response) {
+  type ChirpBody = { body: string };
+  const MAX_CHIRP_LENGTH = 140;
+
+  res.set("Content-Type", "application/json; charset=utf-8");
+
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", () => {
+    try {
+      const parsedBody = JSON.parse(body) as ChirpBody;
+      if (parsedBody.body.length > MAX_CHIRP_LENGTH) {
+        res.status(400).send(JSON.stringify({ "error": "Chirp is too long" }));
+        return;
+      }
+      res.status(200).send(JSON.stringify({ "valid": true }));
+    } catch (error) {
+      res.status(400).send(JSON.stringify({ "error": "Something went wrong" }))
+    }
+  })
 }
