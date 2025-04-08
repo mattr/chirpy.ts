6ec3d47484a67d7b4282c15b4c614dc98c9c9957
Matt Redmond <matt.redmond@katalyst.com.au>
2025-04-09 08:29:15 +0930

    Add custom errors

--- src/handlers/handler-validate-chirp.ts
+++ src/handlers/handler-validate-chirp.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import sanitizeChirp from "../utils/sanitize-chirp.js";
+import BadRequestError from "../errors/bad-request-error.js";
 
 export default async function handlerValidateChirp(req: Request, res: Response) {
   type parameters = { body: string };
@@ -8,7 +9,7 @@ export default async function handlerValidateChirp(req: Request, res: Response) 
   const params: parameters = req.body;
 
   if (params.body.length > MAX_CHIRP_LENGTH) {
-    throw new Error("Chirp is too long");
+    throw new BadRequestError("Chirp is too long. Max length is 140");
   }
 
   res.status(200).json({ cleanedBody: sanitizeChirp(params.body) });
--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import BadRequestError from "../errors/bad-request-error.js";
+import UnauthorizedError from "../errors/unauthorized-error.js";
+import ForbiddenError from "../errors/forbidden-error.js";
+import NotFoundError from "../errors/not-found-error.js";
 
 export default function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.log(err);
-  res.status(500).json({ error: "Something went wrong on our end" });
+  console.log(err.name, err);
+  let error: string;
+  let statusCode: number;
+  if (err instanceof BadRequestError) {
+    error = err.message;
+    statusCode = 400;
+  } else if (err instanceof UnauthorizedError) {
+    error = err.message;
+    statusCode = 401;
+  } else if (err instanceof ForbiddenError) {
+    error = err.message;
+    statusCode = 403;
+  } else if (err instanceof NotFoundError) {
+    error = err.message;
+    statusCode = 404;
+  } else {
+    error = "Something went wrong on our end";
+    statusCode = 500;
+  }
+
+  console.log(error, statusCode);
+  res.status(statusCode).json({ error });
 }
