31f22111695ed5d84d5ec9d010f1196afee067de
Matt Redmond <matt.redmond@katalyst.com.au>
2025-04-11 22:59:33 +0930

    wip auth tests and jwts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export function hashPassword(password: string): string {
   const salt = bcrypt.genSaltSync();
@@ -7,4 +8,27 @@ export function hashPassword(password: string): string {
 
 export function checkPasswordHash(password: string, hash: string): boolean {
   return bcrypt.compareSync(password, hash);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+  type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
+  const payload: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: new Date().getTime(),
+    exp: new Date().getTime() + expiresIn,
+  }
+
+  return jwt.sign(payload, secret);
+}
+
+function validateJWT(tokenString: string, secret: string): string {
+  jwt.decode(tokenString, secret, (err, decoded) => {
+    if (err) {
+      throw err;
+    } else {
+      return decoded.sub;
+    }
+  });
 }
